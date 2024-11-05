ce7043ee8b6078170a65faa3fba6e009643c6663
mstradiot <mstradiot@mstradiot-pc.myguest.virtualbox.org>
2024-11-05 12:37:49 +0100

    Assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,16 +4,31 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int rc;
+
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(1000 * thread_func_args->wait_to_obtain_ms);
+    rc = pthread_mutex_lock(thread_func_args->mutex); 
+    if (rc != 0){
+        DEBUG_LOG("Mutex lock failed to lock with %d", rc);
+	thread_func_args->thread_complete_success = false;
+    }
+    usleep(1000 * thread_func_args->wait_to_release_ms);
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if (rc != 0){
+        DEBUG_LOG("Mutex unlock failed to lock with %d", rc);
+	thread_func_args->thread_complete_success = false;
+    }
+
     return thread_param;
 }
 
@@ -28,6 +43,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* data = malloc(sizeof(struct thread_data));
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = true;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if (rc != 0){
+        DEBUG_LOG("Failed to create a thread %d", rc);
+	data->thread_complete_success = false;
+    }
+
+    return data->thread_complete_success;
 }
 
